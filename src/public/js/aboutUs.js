55f69a71d451bb6b2c2256e36f52aa503c87a27e
duongddat <duongvandat080602@gmail.com>
2023-07-05 16:47:49 +0700

    About us page - full && Fig bug responsive table

---
+++ src/public/js/aboutUs.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var testimonialBodyItems = $(".fui-testimonial-1 .testimonialBodyItem");
+    var testimoniaPersonalItems = $(".fui-testimonial-1 .testimoniaPersonalItem");
+
+    // handle click tab testimonial
+    var numberTabTestimonial = 0;
+
+    testimoniaPersonalItems.on("click", function () {
+        var PersonalTab = $(this).data("tab");
+        numberTabTestimonial = $(this).data("tab");
+        testimoniaPersonalItems.removeClass("active");
+        testimonialBodyItems.removeClass("active");
+        $(this).addClass("active");
+        testimonialBodyItems.each(function () {
+            var itemContent = $(this);
+            if (PersonalTab === itemContent.data("tab")) {
+                $(this).addClass("active");
+            }
+        });
+    });
+
+    setInterval(function () {
+        numberTabTestimonial++;
+        if (numberTabTestimonial > 7) {
+            numberTabTestimonial = 0;
+        }
+        testimoniaPersonalItems.removeClass("active");
+        testimonialBodyItems.removeClass("active");
+        testimoniaPersonalItems.eq(numberTabTestimonial).addClass("active");
+        testimonialBodyItems.eq(numberTabTestimonial).addClass("active");
+    }, 4000);
+});
